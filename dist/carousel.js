4d2429c39b74eeb2b0ff918c70dd043843d80af0
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-06-18 23:07:25 +0200

    some functionality(owl carousel) added

--- dist/carousel.js
+++ dist/carousel.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".barbers__block-wrapper,.gallery__image-wrapper").owlCarousel({
+  $(".barbers__block-wrapper,.gallery__image-wrapper,.clients__block-wrapper").owlCarousel({
         autoplay:true,
         autoplayTimeout:2000,
         nav:true,
@@ -27,6 +27,55 @@ $(document).ready(function () {
 			
 	}
 });
+	$('.clients__block-wrapper').owlCarousel({
+		autoplay:true,
+        autoplayTimeout:2000,
+        nav:true,
+        navText: ["<img src='myprevimage.png'>","<img src='mynextimage.png'>"],
+		loop:true,
+		margin:0,
+		items:1,
+		center:true,
+		merge:true,
+		// responsiveClass:true,
+        // lazyLoad:true,
+		 responsive:{
+				0:{
+					  items:1,
+                      nav:true,
+                      autoHeight:true
+					},
+		520:{
+				items:1,
+				 
+                            nav:true,
+                            autoHeight:true
+						},
+			560:{
+					items:1,
+					  
+                            nav:true,
+                            autoHeight:true
+					},
+		 
+			768:{
+							items:1,
+                            nav:true,
+                            autoHeight:true
+						},
+		992:{
+		    				items:0,
+                            nav:false,
+                            loop:true,
+                            autoplay:true,
+                            autoplayTimeout:2000,
+                            autoplayHoverPause:false,
+                          autoHeight:true
+	},
+			
+	}
+
+	})
 })
 
 
--- src/js/modules/animateNumber.js
+++ src/js/modules/animateNumber.js
@@ -1,25 +1,26 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
-class AnimateNumber {
-    constructor() {
 
-        this.count = $('.count');
-        this.animateNumber();
-    }
-    animateNumber() {
-        $('.count').each(function() {
-            $(this).prop('Counter',0).animate({
-                Counter: $(this).text()
-            }, {
-                duration:4000,
-                easing: 'swing',
-                step: function(now) {
-                    $(this).text(Math.ceil(now));
-                }
-            }
-            );
-        });
-    }
-}
+// class AnimateNumber {
+//     constructor() {
 
-export default AnimateNumber;
+//         this.count = $('.count');
+//         this.animateNumber();
+//     }
+//     animateNumber() {
+//         $('.count').each(function() {
+//             $(this).prop('Counter',0).animate({
+//                 Counter: $(this).text()
+//             }, {
+//                 duration:4000,
+//                 easing: 'swing',
+//                 step: function(now) {
+//                     $(this).text(Math.ceil(now));
+//                 }
+//             }
+//             );
+//         });
+//     }
+// }
+
+// export default AnimateNumber;
