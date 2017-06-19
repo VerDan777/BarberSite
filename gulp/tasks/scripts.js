7fad214123bf5c9bfb0999e515e784e37ef888e8
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-06-19 18:59:47 +0200

    remake some files

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
 $.gulp.task('js',function() {
-   return  $.gulp.src('./src/js/app.js')
+   return  $.gulp.src('./src/js/*.*')
     .pipe($.gp.js())
     .on('error',$.gp.notify.onError(function(error){
        return { 
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@
 
 module.exports = function() {
   $.gulp.task('watch', function() {
-    // $.gulp.watch('./src/js/**/*.js', $.gulp.series('js:process'));
+    $.gulp.watch('./src/js/**/*.js', $.gulp.series('js'));
     $.gulp.watch('./src/sass/**/*.scss', $.gulp.series('sass'));
     $.gulp.watch('./src/*.pug', $.gulp.series('pug'));
     $.gulp.watch('./src/img/**/*.*', $.gulp.series('copyImg'));
