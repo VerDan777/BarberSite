9af1da35124ee5a4c14352a2798a20d98f2343cd
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-06-19 18:59:47 +0200

    remake some files

---
+++ js/carousel.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+  $(".barbers__block-wrapper,.gallery__image-wrapper,.clients__block-wrapper").owlCarousel({
+        autoplay:true,
+        autoplayTimeout:2000,
+        nav:true,
+        navText: ["<img src='myprevimage.png'>","<img src='mynextimage.png'>"],
+		loop:true,
+		margin:20,
+        lazyLoad:true,
+		 responsive:{
+				0:{
+				items:1,
+					},
+		520:{
+				items:1,
+						},
+			560:{
+					items:2,
+					},
+		 
+			768:{
+		items:2,
+						},
+		992:{
+			items:3,
+	},
+			
+	}
+});
+	$('.clients__block-wrapper').owlCarousel({
+		autoplay:true,
+        autoplayTimeout:2000,
+        nav:true,
+        navText: ["<img src='myprevimage.png'>","<img src='mynextimage.png'>"],
+		loop:true,
+		margin:0,
+		items:1,
+		center:true,
+		merge:true,
+		// responsiveClass:true,
+        // lazyLoad:true,
+		 responsive:{
+				0:{
+					  items:1,
+                      nav:true,
+                      autoHeight:true
+					},
+		520:{
+				items:1,
+				 
+                            nav:true,
+                            autoHeight:true
+						},
+			560:{
+					items:1,
+					  
+                            nav:true,
+                            autoHeight:true
+					},
+		 
+			768:{
+							items:1,
+                            nav:true,
+                            autoHeight:true
+						},
+		992:{
+		    				items:0,
+                            nav:false,
+                            loop:true,
+                            autoplay:true,
+                            autoplayTimeout:2000,
+                            autoplayHoverPause:false,
+                          autoHeight:true
+	},
+			
+	}
+
+	})
+})
+
+
