2c5fcf25e04536290ba703a7d6bfec5d76e4de11
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-10-22 18:45:36 +0200

    commit

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+import MobileMenu from './modules/MobileMenu.js';
+import AnimateNumber from './modules/animateNumber.js';
+import OwlCarousel from './modules/OwlCarousel.js';
+import Waypoints from './modules/waypoint.js';
+import MagnificPopup from './modules/magnificpopup.js';
+
+
+const mobilemenu = new MobileMenu();
+const animateNumber = new AnimateNumber();
+const owlcarousel = new OwlCarousel();
+const waypoint = new Waypoints();
+const magnificpopup = new MagnificPopup();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IE1vYmlsZU1lbnUgZnJvbSAnLi9tb2R1bGVzL01vYmlsZU1lbnUuanMnO1xuaW1wb3J0IEFuaW1hdGVOdW1iZXIgZnJvbSAnLi9tb2R1bGVzL2FuaW1hdGVOdW1iZXIuanMnO1xuaW1wb3J0IE93bENhcm91c2VsIGZyb20gJy4vbW9kdWxlcy9Pd2xDYXJvdXNlbC5qcyc7XG5pbXBvcnQgV2F5cG9pbnRzIGZyb20gJy4vbW9kdWxlcy93YXlwb2ludC5qcyc7XG5pbXBvcnQgTWFnbmlmaWNQb3B1cCBmcm9tICcuL21vZHVsZXMvbWFnbmlmaWNwb3B1cC5qcyc7XG5cblxuY29uc3QgbW9iaWxlbWVudSA9IG5ldyBNb2JpbGVNZW51KCk7XG5jb25zdCBhbmltYXRlTnVtYmVyID0gbmV3IEFuaW1hdGVOdW1iZXIoKTtcbmNvbnN0IG93bGNhcm91c2VsID0gbmV3IE93bENhcm91c2VsKCk7XG5jb25zdCB3YXlwb2ludCA9IG5ldyBXYXlwb2ludHMoKTtcbmNvbnN0IG1hZ25pZmljcG9wdXAgPSBuZXcgTWFnbmlmaWNQb3B1cCgpOyJdLCJmaWxlIjoiYXBwLmpzIn0=
